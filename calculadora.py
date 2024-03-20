b4ca237df806bbc317ea347bc6abe9a84a171b27
BabbingtonnJR <162913182+BabbingtonnJR@users.noreply.github.com>
2024-03-20 11:23:57 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,37 @@
-num1 = float(input('Digite um número: '))
-num2 = float(input('Digite um segundo número: '))
-
-print(f'1:SOMA \n 2:SUBTRAÇÃO \n 3:MULTIPLICAÇÃO \n 4:DIVISÃO \n 5:SAIR')
+print(f'1:SOMA\n2:SUBTRAÇÃO\n3:MULTIPLICAÇÃO\n4:DIVISÃO\n5:SAIR')
 
 operacao = int(input('Escolha a operação matemática: '))
 
-if operacao == 1:
-    print(f'O resultado da operação é:\n {num1} + {num2} = {num1 + num2}')
-elif operacao == 2:
-    print(f'O resultado da operação é:\n {num1} - {num2} = {num1 - num2}')
-elif operacao == 3:
-    print(f'O resultado da operação é:\n {num1} x {num2} = {num1 * num2}')
-elif operacao == 4:
-    print(f'O resultado da operação é:\n {num1} / {num2} = {num1 / num2}')
-elif operacao == 5:
-    print(f'Você saiu da calculadora')
+while (operacao) != 5:
+    if operacao == 1:
+        num1 = float(input('Digite um número: '))
+        num2 = float(input('Digite um segundo número: ')) 
+        print(f'\nO resultado da operação é:\n {num1} + {num2} = {num1 + num2}\n\n')
+        print(f'1:SOMA\n2:SUBTRAÇÃO\n3:MULTIPLICAÇÃO\n4:DIVISÃO\n5:SAIR')
+        operacao = int(input('Escolha a operação matemática: '))
+    elif operacao == 2:
+        num1 = float(input('Digite um número: '))
+        num2 = float(input('Digite um segundo número: ')) 
+        print(f'\nO resultado da operação é:\n {num1} - {num2} = {num1 - num2}\n\n')
+        print(f'1:SOMA\n2:SUBTRAÇÃO\n3:MULTIPLICAÇÃO\n4:DIVISÃO\n5:SAIR')
+        operacao = int(input('Escolha a operação matemática: '))
+    elif operacao == 3:
+        num1 = float(input('Digite um número: '))
+        num2 = float(input('Digite um segundo número: ')) 
+        print(f'\nO resultado da operação é:\n {num1} * {num2} = {num1 * num2}\n\n')
+        print(f'1:SOMA\n2:SUBTRAÇÃO\n3:MULTIPLICAÇÃO\n4:DIVISÃO\n5:SAIR')
+        operacao = int(input('Escolha a operação matemática: '))
+    elif operacao == 4:
+        num1 = float(input('Digite um número: '))
+        num2 = float(input('Digite um segundo número: ')) 
+        print(f'\nO resultado da operação é:\n {num1} / {num2} = {num1 / num2}\n\n')
+        print(f'1:SOMA\n2:SUBTRAÇÃO\n3:MULTIPLICAÇÃO\n4:DIVISÃO\n5:SAIR')
+        operacao = int(input('Escolha a operação matemática: '))
+    elif operacao > 5:
+        print('\nEscolha uma das opções validas\n')
+        print(f'1:SOMA\n2:SUBTRAÇÃO\n3:MULTIPLICAÇÃO\n4:DIVISÃO\n5:SAIR')
+        operacao = int(input('Escolha a operação matemática: ')) 
+    elif operacao == 5:
+        break
 else:
-    print(f'Comando inválido')
+    print(f'Você saiu da calculadora')
