a337c6fff2a9672d3fc1bc2e87bff98bdcbce211
BabbingtonnJR <162913182+BabbingtonnJR@users.noreply.github.com>
2024-04-03 09:57:37 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -2,24 +2,19 @@ operacao = int()
 while (operacao) != 5:
     print(f'1:SOMA\n2:SUBTRAÇÃO\n3:MULTIPLICAÇÃO\n4:DIVISÃO\n5:SAIR')
     operacao = int(input('Escolha a operação matemática: '))
+    if operacao == 5:
+        print(f'Você saiu da calculadora!')
+        break
+    num1 = float(input('Digite um número: '))
+    num2 = float(input('Digite um segundo número: ')) 
     if operacao == 1:
-        num1 = float(input('Digite um número: '))
-        num2 = float(input('Digite um segundo número: ')) 
         print(f'\nO resultado da operação é:\n {num1} + {num2} = {num1 + num2}\n\n')
     elif operacao == 2:
-        num1 = float(input('Digite um número: '))
-        num2 = float(input('Digite um segundo número: ')) 
         print(f'\nO resultado da operação é:\n {num1} - {num2} = {num1 - num2}\n\n')
-    elif operacao == 3:
-        num1 = float(input('Digite um número: '))
-        num2 = float(input('Digite um segundo número: ')) 
+    elif operacao == 3: 
         print(f'\nO resultado da operação é:\n {num1} * {num2} = {num1 * num2}\n\n')
     elif operacao == 4:
-        num1 = float(input('Digite um número: '))
-        num2 = float(input('Digite um segundo número: ')) 
         print(f'\nO resultado da operação é:\n {num1} / {num2} = {num1 / num2}\n\n')
     elif operacao > 5:
         print('\nEscolha uma das opções validas\n')
-    elif operacao == 5:
-        print('Você saiu da calculadora')
-        break
+        continue
